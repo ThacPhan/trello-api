75885988967ca0836c7fb297d349f49271fa33b5
Thac Phan <phandinhthac98@gmail.com>
2024-07-15 07:50:19 +0700

    setup project/env variable

--- src/server.js
+++ src/server.js
@@ -3,18 +3,20 @@ import express from "express";
 import { env } from '~/config/environment';
 import { CLOSE_DB, CONNECT_DB } from "~/config/mongodb";
 
+// khởi chạy db
 const START_SERVER = () => {
   const app = express();
-  
+
   app.get("/", async(req, res) => {
     res.end("<h1>Hello Trello Website!</h1><hr>")
   })
-  
+
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
     console.log(`Hello ${env.AUTHOR} Trello Web, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`);
   })
 
+  // đóng db
   exitHook(() => {
     console.log('Server is shutting down...')
     CLOSE_DB();
@@ -22,12 +24,12 @@ const START_SERVER = () => {
 }
 
 (async () => {
-  try{
+  try {
     console.log('1 Connecting to MongoDB Cloud Atlas...')
     await CONNECT_DB()
     console.log('Connected to MongoDB Cloud Atlas!')
     START_SERVER()
-  }catch(error){
+  } catch (error) {
     console.error(error)
     process.exit(0)
   }
