f2439d74e384f3d92b9949bbd5a21dd9bc95b49e
Thac Phan <phandinhthac98@gmail.com>
2024-07-18 09:02:04 +0700

    push code

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,1 +1,16 @@
+import { StatusCodes } from 'http-status-codes';
 
+const createNew = async (req, res) => {
+  try {
+    console.log('req.body:', req.body)
+    res.status(StatusCodes.CREATED).json( { message: 'Post APIs list boards' })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: error.message
+    })
+  }
+}
+
+export const boardController = {
+  createNew
+}
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,17 +1,14 @@
 import { StatusCodes } from 'http-status-codes';
 import Joi from 'joi';
 
-const createNew = async (req, res) => {
+const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict(),
     description: Joi.string().required().min(3).max(256).trim().strict()
   })
   try {
-    console.log('req.body:', req.body)
     await correctCondition.validateAsync(req.body, { abortEarly: false })
-
-    res.status(StatusCodes.CREATED).json( { message: 'Get APIs list boards' })
-    // next()
+    next()
   } catch (error) {
     console.log(error)
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
